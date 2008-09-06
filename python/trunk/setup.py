cbc0b3114c248ef3836b2460500a6c95d7c35523
Grammarian <Grammarian@0bec5ed8-b53f-49e6-9885-ce7bc93af311>
2008-09-06 19:22:02 +0000

    - v1.2 release

--- python/trunk/setup.py
+++ python/trunk/setup.py
@@ -1,12 +1,12 @@
 import sys
 
 NAME = "ObjectListView"
-VERSION = "1.1"
+VERSION = "1.2"
 URL = "http://objectlistview.sourceforge.net/python"
-DOWNLOAD_URL = "http://objectlistview.sourceforge.net/samples/ObjectListView-1.1.zip"
+DOWNLOAD_URL = "http://objectlistview.sourceforge.net/samples/ObjectListView-1.2.zip"
 LICENSE = "wxWindows"
 AUTHOR = "Phillip Piper"
-AUTHOR_EMAIL = "phillip_piper@bigfoot.com"
+AUTHOR_EMAIL = "phillip.piper@gmail.com"
 YEAR = 2008
 DESCRIPTION = "An ObjectListView is a wrapper around the wx.ListCtrl that makes the list control easier to use"
 
@@ -20,20 +20,23 @@ list control easier to use. It also provides some useful extra functionality.
 
 * Automatically transforms a collection of model objects into a fully functional wx.ListCtrl.
 * Automatically sorts rows.
-* Easily edits the cell values.
+* Easily edit the cell values.
 * Supports all ListCtrl views (report, list, large and small icons).
-* Columns can be fixed-width, have a minimum and/or maximum width, or be space-filling.
-* Displays a "list is empty" message when the list is empty (obviously).
-* Supports checkboxes in any column
+* Columns can be fixed-width, have a minimum and/or maximum width, or be space-filling
+* Displays a "list is empty" message  when the list is empty (obviously).
+* Supports checkboxes in any column.
 * Supports alternate rows background colors.
-* Supports custom formatting of rows.
+* Supports custom formatting of rows .
 * Supports searching (by typing) on any column, even on massive lists.
 * Supports custom sorting
-* The `FastObjectListView` version can build a list of 10,000 objects in less than 0.1 seconds.
-* The `VirtualObjectListView` version supports millions of rows through ListCtrl's virtual mode.
+* Supports filtering and batched updates
+* The ``FastObjectListView`` version can build a list of 10,000 objects in less than 0.1 seconds.
+* The ``VirtualObjectListView`` version supports millions of rows through ListCtrl's virtual mode.
+* The ``GroupListView`` version supports arranging rows into collapsible groups.
+* Effortlessly produce professional-looking reports using a ``ListCtrlPrinter``.
 
-It also has a 'GroupListView' which is a ListCtrl that allows the rows to be grouped into
-collapsible partitions.
+Seriously, after using an ObjectListView, you will never go back to using a plain wx.ListCtrl.
+
 
 Dependancies
 ============
